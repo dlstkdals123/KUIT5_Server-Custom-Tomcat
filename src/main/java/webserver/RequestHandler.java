f8cd46a87e3422ab57fe06841538e39d3c918a4b
dlstkdals123 <dlstkdals123@naver.com>
2025-03-30 10:19:44 +0900

    chore: 요구사항2: 회원가입으로 인스턴스 등록 및 index.html 보여주기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,9 +1,15 @@
 package webserver;
 
+import db.MemoryUserRepository;
+import http.util.HttpRequestUtils;
+import model.User;
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,11 +31,34 @@ public class RequestHandler implements Runnable{
             String[] tokens = br.readLine().split(" ");
             if (tokens[1].equals("/"))
                 tokens[1] = "/index.html";
-            String filePath = "webapp" + tokens[1];
-            byte[] body = Files.readAllBytes(Paths.get(filePath));
 
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            if (tokens[1].endsWith(".html")) {
+                String filePath = "webapp" + tokens[1];
+                byte[] body = Files.readAllBytes(Paths.get(filePath));
+
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+
+            else { // queryString
+                String[] querySplit = tokens[1].split("\\?");
+                System.out.println(tokens[1]);
+                String endpoint = querySplit[0];
+                String queryString = querySplit[1];
+
+                if (endpoint.equals("/user/signup")) {
+                    HashMap<String, String> params = (HashMap<String, String>) HttpRequestUtils.parseQueryParameter(queryString);
+                    MemoryUserRepository memoryUserRepository = MemoryUserRepository.getInstance();
+                    memoryUserRepository.addUser(new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email")));
+
+                    String filePath = "webapp/index.html";
+                    byte[] body = Files.readAllBytes(Paths.get(filePath));
+
+                    response200Header(dos, body.length);
+                    responseBody(dos, body);
+                }
+            }
+
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
